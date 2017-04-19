df890877e99167e43a5a249789773cec22397df2
Robert McCoy <rmccoy6435@gmail.com>
2017-04-19 13:22:58 -0400

    Added in JavaScript for generating templates
    Can generate blog lists based on templates and remove things from it
    under preferences

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,36 +1,60 @@
 const apiKey = config.API_Key;
 
 function readFeeds() {
-	var blogFeeds = ["https://www.alvinashcraft.com/feed/", "https://blog.codinghorror.com/rss/", "https://visualstudiomagazine.com/rss-feeds/news.aspx", "https://visualstudiomagazine.com/rss-feeds/practical-net.aspx"];
-	blogFeeds.forEach(function(element) {
-		$.ajax({
-			url: 'https://api.rss2json.com/v1/api.json',
-			method: 'GET',
-			dataType: 'json',
-			data: {
-				rss_url: element,
-				api_key: apiKey,
-				count: 5
-			}
-		}).done(function(response) {
-			if (response.status != 'ok') {
-				throw response.message;
-			}
-			var tableRows = generateTableRows(response);
-			var newDiv = document.createElement('div');
-			document.getElementById('tables').appendChild(newDiv);
-			newDiv.id = 'div' + replaceAll(response.feed.title, " ", "")
-			newDiv.innerHTML = '<h1>' + response.feed.title + '</h1><h4>' + response.feed.description + ' </h4><br/>' +
-								'<table class="table table-hover table-bordered table-striped">' +
-									'<tr>' +
-										'<th>Title</th>' +
-										'<th>Published</th>' +
-										'<th>Author</th>' +
-									'</tr>' +
-									tableRows +
-								'</table>';
-		});
-	});
+	if (localStorage.getItem('blogs') !== null && localStorage.getItem('blogs').length !== 0) {
+        JSON.parse(localStorage.getItem('blogs')).forEach(function (element) {
+            $.ajax({
+                url: 'https://api.rss2json.com/v1/api.json',
+                method: 'GET',
+                dataType: 'json',
+                data: {
+                    rss_url: element,
+                    api_key: apiKey,
+                    count: 5
+                }
+            }).done(function (response) {
+                if (response.status != 'ok') {
+                    throw response.message;
+                }
+                var tableRows = generateTableRows(response);
+                var newDiv = document.createElement('div');
+                document.getElementById('tables').appendChild(newDiv);
+                newDiv.id = 'div' + replaceAll(response.feed.title, " ", "")
+                newDiv.innerHTML = '<h1>' + response.feed.title + '</h1><h4>' + response.feed.description + ' </h4><br/>' +
+                    '<table class="table table-hover table-bordered table-striped">' +
+                    '<tr>' +
+                    '<th>Title</th>' +
+                    '<th>Published</th>' +
+                    '<th>Author</th>' +
+                    '</tr>' +
+                    tableRows +
+                    '</table>';
+            });
+        });
+    }
+}
+
+function generatePreferenceBlogs() {
+	var table = "<table class='table table-hover table-bordered table-responsive'><tr><th>Feed URL</th><th></th></tr>";
+	JSON.parse(localStorage.getItem('blogs')).forEach(function (element) {
+		table += "<tr><td>" + element + "</td><td><input type='button' onclick='deleteFeed(\"" + element + "\")' class='btn btn-warning' value='Delete' /></td></tr>";
+	})
+	table += "</table>";
+	document.getElementById('blogTable').innerHTML = table;
+}
+
+function deleteFeed(feed) {
+	var feeds = JSON.parse(localStorage.getItem('blogs'));
+	var indexToRemove;
+	for (var i = 0; i < feeds.length; i++) {
+		if (feeds[i] === feed) {
+			indexToRemove = i;
+			break;
+		}
+	}
+	feeds.splice(indexToRemove, 1);
+	localStorage.setItem('blogs', JSON.stringify(feeds))
+	location.reload();
 }
 
 function generateTableRows(response) {
